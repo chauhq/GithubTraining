da4928b00ec054021e2ff16fe38b7f86110c9482
Chau Hoang Q <hquangchau@gmail.com>
2025-03-28 07:42:00 +0700

    remove

--- app/src/main/java/hqc/com/tranningapplication/MainActivity.kt
+++ app/src/main/java/hqc/com/tranningapplication/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                    // helllo
                 }
             }
         }
